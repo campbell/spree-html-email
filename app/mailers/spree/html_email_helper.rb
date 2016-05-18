a22d71c077129f5348b3dff6caa9baa31d21e5c4
Pete Campbell <pete@sumirolabs.com>
2016-05-18 12:19:56 -0400

    Bugfix to handle USPS tracking numbers

--- app/mailers/spree/html_email_helper.rb
+++ app/mailers/spree/html_email_helper.rb
@@ -5,5 +5,15 @@ module Spree
       link_to(text || tracking_number, "http://wwwapps.ups.com/WebTracking/processInputRequest?sort_by=status&tracknums_displayed=1&TypeOfInquiryNumber=T&loc=en_us&InquiryNumber1=#{tracking_number}&track.x=0&track.y=0", class: 'track_shipment', target: '_blank') if tracking_number.present?
     end
 
+    def tracking_link(tracking_number, text = nil)
+      link_url = if tracking_number =~ /1z/i
+        "http://wwwapps.ups.com/WebTracking/processInputRequest?sort_by=status&tracknums_displayed=1&TypeOfInquiryNumber=T&loc=en_us&InquiryNumber1=#{tracking_number}&track.x=0&track.y=0"
+      else
+        "https://tools.usps.com/go/TrackConfirmAction.action?tLc=1&tLabels=#{tracking_number}"
+      end
+  
+      link_to(text || tracking_number, link_url, class: 'track_shipment', target: '_blank') if tracking_number.present?
+    end
+
   end
 end
